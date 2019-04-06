c848134317078e9170565b42bf7a62c565e01101
Phil Huang <42421010+philhuang99@users.noreply.github.com>
2019-04-05 22:43:53 -0400

    Update Drum AR.py

--- Drum AR.py
+++ Drum AR.py
@@ -10,15 +10,21 @@ colorUpper = np.array([60, 255, 255], np.uint8)
 cap = cv2.VideoCapture(0)
 
 while(True):
+    #ret=True if frame is available, frame will actually capture the frame
     ret, frame = cap.read()
+
     frame = cv2.resize(frame, (0,0), fx = 2, fy = 2)
     frame = cv2.flip(frame, +1)
     frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
     colorMask = cv2.inRange(frameHSV, colorLower, colorUpper)
     res = cv2.bitwise_and(frame, frame, mask = colorMask)
+    
+    #creating a frame
     cv2.imshow("Hello", res)
     cv2.imshow("Drum AR", frame)
-    #if condition is met, break out of loop
+        
+    #if condition is met, break out of loop at keyPress "q"
     ch = cv2.waitKey(1)
     if ch & 0xFF == ord('q'):
         break
